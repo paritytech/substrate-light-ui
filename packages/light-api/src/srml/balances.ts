687490c2c89fd585a7f087b7353031d4d9bd71d4
Amaury Martiny <amaury.martiny@protonmail.com>
2018-12-06 15:29:26 +0100

    Add timestamp blockPeriod

--- packages/light-api/src/srml/balances.ts
+++ packages/light-api/src/srml/balances.ts
@@ -14,10 +14,10 @@ import { addEventNode } from '../utils/addEventNode';
 const SECTION = 'balances';
 
 export function transferFee (graph: ReactiveGraph, api: ApiRx) {
-  // Create a transferFeeNode from the newHeadNode, with a pipe.
-  const transferFeeNode = 'query.balances.transferFee()';
-  graph.setNode(transferFeeNode);
-  graph.setReactiveEdge(NODES.NEW_HEAD, transferFeeNode, [switchMap(() => api.query.balances.transferFee())]);
+  const myNode = `query.${SECTION}.transferFee()`;
+  graph.setNode(myNode);
+  graph.setReactiveEdge(NODES.NEW_HEAD, myNode, [switchMap(() => api.query.balances.transferFee())]);
+  return graph.node(myNode);
 }
 
 export function freeBalance (accountId: AccountId, graph: ReactiveGraph, api: ApiRx) {
@@ -36,9 +36,9 @@ export function freeBalance (accountId: AccountId, graph: ReactiveGraph, api: Ap
   graph.setReactiveEdge(eventNode2, mergedNode);
 
   // Create a freeBalanceNode from the mergedNode, with a pipe.
-  const freeBalanceNode = `query.balances.freeBalance(${accountId})`;
-  graph.setNode(freeBalanceNode);
-  graph.setReactiveEdge(NODES.EVENTS, freeBalanceNode, [switchMap(() => api.query.balances.freeBalance(accountId))]);
+  const myNode = `query.${SECTION}.freeBalance(${accountId})`;
+  graph.setNode(myNode);
+  graph.setReactiveEdge(NODES.EVENTS, myNode, [switchMap(() => api.query.balances.freeBalance(accountId))]);
 
-  return graph.node(freeBalanceNode);
+  return graph.node(myNode);
 }
