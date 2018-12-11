1d7aa2e5cd3e568f4e85cece16c31155e8710eba
YJ <yjkimjunior@gmail.com>
2018-12-10 18:29:58 -0800

    bump dependency versions, tslint

---
+++ packages/ui-components/src/i18n.ts
@@ -0,0 +1,30 @@
+// Copyright 2017-2018 @polkadot/light-apps authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-xhr-backend';
+import { reactI18nextModule } from 'react-i18next';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(reactI18nextModule)
+  .init({
+    backend: {
+      loadPath: 'locales/{{lng}}/{{ns}}.json'
+    },
+    debug: false,
+    defaultNS: 'ui',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    },
+    ns: ['ui'],
+    react: {
+      wait: true
+    }
+  });
+
+export default i18n;
