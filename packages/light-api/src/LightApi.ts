b397e7aef188eacca56ca3c42fb8a2912c062f47
Amaury Martiny <amaury.martiny@protonmail.com>
2018-12-15 23:33:15 +0100

    Re-add light-api

---
+++ packages/light-api/src/LightApi.ts
@@ -0,0 +1,55 @@
+// Copyright 2017-2018 @polkadot/light-api authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import ApiRx from '@polkadot/api/rx';
+import WsProvider from '@polkadot/rpc-provider/ws';
+import { of } from 'rxjs';
+import { mergeAll, switchMap } from 'rxjs/operators';
+
+import { ReactiveGraph } from './ReactiveGraph';
+import * as balances from './srml/balances';
+
+interface LightFunctions {
+  [index: string]: any; // TODO Better types here
+}
+
+// Some important nodes in the Graph
+export const NODES = {
+  EVENTS: 'query.system.events',
+  NEW_HEAD: 'rpc.chain.subscribeNewHead',
+  STARTUP: 'startup'
+};
+
+export class LightApi extends ApiRx {
+  public graph: ReactiveGraph = new ReactiveGraph();
+  public light: LightFunctions;
+
+  constructor (wsProvider?: WsProvider) {
+    super(wsProvider);
+
+    // Add basic source nodes
+    this.graph.setNode(NODES.STARTUP, of(1));
+    this.graph.setNode(NODES.NEW_HEAD, this.rpc.chain.subscribeNewHead());
+
+    // Add events node
+    this.graph.setNode(NODES.EVENTS);
+    this.graph.setEdge(NODES.NEW_HEAD, NODES.EVENTS, [
+      switchMap(() => this.query.system.events()),
+      // Each "event" in `query.system.events` is actually a Vector of
+      // EventRecord, we mergeAll here as to have an Observable<EventRecord>.
+      mergeAll()
+    ]);
+    this.graph.calculateNode(NODES.EVENTS);
+
+    this.light = Object.keys(balances).reduce(
+      (result, key) => {
+        // @ts-ignore
+        result[key] = (...args: any[]) => balances[key](...args, this.graph, this);
+
+        return result;
+      },
+      {} as LightFunctions
+    );
+  }
+}
