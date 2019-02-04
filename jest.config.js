528aa982d2d75ba4de8d5ae77fccf3b14766dcd8
Amaury Martiny <amaury.martiny@protonmail.com>
2019-02-04 21:36:35 +0100

    Remove ui-api

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,1 @@
-const baseConfig = require('@polkadot/dev/config/jest');
-
-module.exports = {
-  ...baseConfig,
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
-  }
-}
+module.exports = require('@polkadot/dev/config/jest');
