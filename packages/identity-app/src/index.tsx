bc6d00d8b0811e2345b5383033ef4b0a8d1f7408
Amaury Martiny <amaury.martiny@protonmail.com>
2019-02-06 13:03:46 +0100

    Fix keyring loadAll

--- packages/identity-app/src/index.tsx
+++ packages/identity-app/src/index.tsx
@@ -2,4 +2,9 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import keyring from '@polkadot/ui-keyring';
+
+// FIXME Why do I need to loadAll again here???
+keyring.loadAll();
+
 export * from './Identity';
