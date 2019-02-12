464ff03f9612993c6904fc3017a015e5448a9163
Amaury Martiny <amaury.martiny@protonmail.com>
2019-02-12 16:25:54 +0100

    First pass

---
+++ packages/electron-app/src/main/util/staticPath.ts
@@ -0,0 +1,17 @@
+// Copyright 2018-2019 @paritytech/substrate-light-ui authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+declare var __static: string;
+
+// Is our app packaged in a binary or still in Electron?
+const appIsPackaged = !process.defaultApp;
+
+/**
+ * Get the path to the `static` folder.
+ *
+ * @see https://github.com/electron-userland/electron-webpack/issues/52
+ */
+export const staticPath = appIsPackaged
+  ? __dirname.replace(/app\.asar$/, 'static')
+  : __static;
