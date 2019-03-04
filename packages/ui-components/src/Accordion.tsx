4289a01b8a3fecee125e387542542d8a7649ebc4
YJ <yjkimjunior@gmail.com>
2019-03-04 14:39:20 +0800

    storybook addons

--- packages/ui-components/src/Accordion.tsx
+++ packages/ui-components/src/Accordion.tsx
@@ -2,17 +2,16 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import SUIAccordion from 'semantic-ui-react/dist/commonjs/modules/Accordion';
 
 type Props = {
   [index: string]: any
 };
 
-export default class Accordion extends React.PureComponent<Props> {
+export default class Accordion extends PureComponent<Props> {
   static Accordion = SUIAccordion.Accordion;
   static Content = SUIAccordion.Content;
-  static Panel = SUIAccordion.Panel;
   static Title = SUIAccordion.Title;
 
   render () {
--- packages/ui-components/stories/Accordion.stories.tsx
+++ packages/ui-components/stories/Accordion.stories.tsx
@@ -2,31 +2,24 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 import React from 'react';
+import { Icon } from 'semantic-ui-react';
 import { withKnobs, text, select, boolean, object } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 
-import { Accordion, Icon } from '../src';
+import { Accordion } from '../src';
 
 storiesOf('Accordion', module)
   .addDecorator(withKnobs)
   .add('inactive with no children or title', () => (
     <Accordion />
   ))
-  .add('inactive with title', () => (
-    <Accordion>
-      <Accordion.Title> Awesome Title </Accordion.Title>
-    </Accordion>
-  ))
   .add('inactive with content and title', () => (
-    <Accordion styled>
-      <Accordion.Title active={false}>
-        <Icon name='dropdown' />
+    <Accordion fluid inverted styled>
+      <Accordion.Title active={boolean('active', false)}>
+        <Icon name='dropdown' /> Title Goes Here
       </Accordion.Title>
-      <Accordion.Content>
+      <Accordion.Content active={boolean('active', false)}>
         Hello this is my content
       </Accordion.Content>
     </Accordion>
-  ))
-  .add('active with child', () => (
-    <Accordion active> hello </Accordion>
   ));
