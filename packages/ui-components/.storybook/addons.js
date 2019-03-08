791517b459aa5cdd6a262bf8f6f7af5d340c04f6
YJ <yjkimjunior@gmail.com>
2019-03-08 14:31:27 +0800

    add knobs

--- packages/ui-components/.storybook/addons.js
+++ packages/ui-components/.storybook/addons.js
@@ -2,11 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { configure } from '@storybook/react';
-const req = require.context('../stories', true, /.stories.tsx$/);
-
-function loadStories () {
-  req.keys().forEach(req);
-}
-
-configure(loadStories, module);
+import '@storybook/addon-actions/register';
+import '@storybook/addon-knobs/register';
+import '@storybook/addon-links/register';
--- packages/ui-components/stories/Address.stories.tsx
+++ packages/ui-components/stories/Address.stories.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text } from '@storybook/addon-knobs/react';
+
 import Address from '../src/Address/';
 
 storiesOf('Address Segment', module)
+  .addDecorator(withKnobs)
   .add('with Placeholder Address', () => (
     <Address />
   ))
   .add('with address', () => (
-    <Address address={'5GeJHN5EcUGPoa5pUwYkXjymoDVN1DJHsBR4UGX4XRAwKBVk'} />
+    <Address address={text('address', '5GeJHN5EcUGPoa5pUwYkXjymoDVN1DJHsBR4UGX4XRAwKBVk')} />
   ));
