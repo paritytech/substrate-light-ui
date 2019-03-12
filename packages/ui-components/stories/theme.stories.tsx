a6ee5dc18e440435c60a90fdd6974c77b29e2654
YJ <yjkimjunior@gmail.com>
2019-03-12 13:28:33 +0800

    typography, typescale

--- packages/ui-components/stories/theme.stories.tsx
+++ packages/ui-components/stories/theme.stories.tsx
@@ -7,10 +7,10 @@ import { action } from '@storybook/addon-actions';
 import { boolean, select, text, withKnobs } from '@storybook/addon-knobs';
 
 import { substrateLightTheme } from '../src/globalStyle';
-import { Card, Container, Grid } from '../src';
+import { Card, Container, DynamicSizeText, ErrorText, FadedText, Grid, Header, SubHeader, SuccessText } from '../src';
 
 const colorPaletteCard = (color, hex) => (
-  <Card>
+  <Card style={{ flex: `1 0 calc(33.333% - 20px)`, maxWidth: `calc(33.333% - 20px)` }}>
     <div style={{ background: hex, width: '100%', height: '85%' }} />
     <Card.Content style={{ display: 'flex column', textAlign: 'center' }}>
       <Card.Header> <b>Color:</b> {color} </Card.Header>
@@ -20,16 +20,63 @@ const colorPaletteCard = (color, hex) => (
 );
 
 storiesOf('Theme', module)
+  .addDecorator(withKnobs)
   .add('colors', () => (
+    <Container style={{ display: 'flex', flexFlow: 'row wrap', width: '100%' }}>
+      {
+        Object.entries(substrateLightTheme).map(([color, hex]) => {
+          return colorPaletteCard(color, hex);
+        })
+      }
+    </Container>
+  ))
+  .add('typescale', () => (
     <Container>
-      <Grid>
-        <Grid.Row style={{ display: 'flex', alignItems: 'space-between' }}>
-          {
-            Object.entries(substrateLightTheme).map(([color, hex]) => {
-              return colorPaletteCard(color, hex);
-            })
-          }
-        </Grid.Row>
-      </Grid>
+      <Header> Header </Header>
+      <SubHeader> SubHeader </SubHeader>
+      <SuccessText> Success </SuccessText>
+      <ErrorText> Error </ErrorText>
+      <FadedText> Faded </FadedText>
+      <DynamicSizeText
+        fontSize={text('fontSize', '14px')}
+        fontWeight={text('fontWeight', '500')}>
+          Dynamic
+      </DynamicSizeText>
+    </Container>
+  ))
+  .add('typography', () => (
+    <Container>
+      <div style={{ display: 'flex' }}>
+        <div style={{ flex: 1 }}>
+          <h2 style={{ fontFamily: 'Segoe UI' }}> Segoe UI </h2>
+          <h2 style={{ fontFamily: 'Roboto' }}> Roboto </h2>
+          <h2 style={{ fontFamily: 'Oxygen' }}> Oxygen </h2>
+          <h2 style={{ fontFamily: 'Ubuntu' }}> Ubuntu </h2>
+          <h2 style={{ fontFamily: 'Cantarell' }}> Cantarell </h2>
+          <h2 style={{ fontFamily: 'Fira Sans' }}> Fira Sans </h2>
+          <h2 style={{ fontFamily: 'Droid Sans' }}> Droid Sans </h2>
+          <h2 style={{ fontFamily: 'Helvetica Neue' }}> Helvetica Neue </h2>
+        </div>
+        <div style={{ flex: 1 }}>
+          <h5 style={{ fontFamily: 'Segoe UI' }}> Segoe UI </h5>
+          <h5 style={{ fontFamily: 'Roboto' }}> Roboto </h5>
+          <h5 style={{ fontFamily: 'Oxygen' }}> Oxygen </h5>
+          <h5 style={{ fontFamily: 'Ubuntu' }}> Ubuntu </h5>
+          <h5 style={{ fontFamily: 'Cantarell' }}> Cantarell </h5>
+          <h5 style={{ fontFamily: 'Fira Sans' }}> Fira Sans </h5>
+          <h5 style={{ fontFamily: 'Droid Sans' }}> Droid Sans </h5>
+          <h5 style={{ fontFamily: 'Helvetica Neue' }}> Helvetica Neue </h5>
+        </div>
+        <div style={{ flex: 1 }}>
+          <p style={{ fontFamily: 'Segoe UI' }}> Segoe UI </p>
+          <p style={{ fontFamily: 'Roboto' }}> Roboto </p>
+          <p style={{ fontFamily: 'Oxygen' }}> Oxygen </p>
+          <p style={{ fontFamily: 'Ubuntu' }}> Ubuntu </p>
+          <p style={{ fontFamily: 'Cantarell' }}> Cantarell </p>
+          <p style={{ fontFamily: 'Fira Sans' }}> Fira Sans </p>
+          <p style={{ fontFamily: 'Droid Sans' }}> Droid Sans </p>
+          <p style={{ fontFamily: 'Helvetica Neue' }}> Helvetica Neue </p>
+        </div>
+      </div>
     </Container>
   ));
