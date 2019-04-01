b52fd0d2f7d42e499c82fb6aeb7b9be4b8c09f7b
YJ <yjkimjunior@gmail.com>
2019-04-01 15:39:59 +0800

    feat: story

--- packages/identity-app/src/Identity.tsx
+++ packages/identity-app/src/Identity.tsx
@@ -6,27 +6,65 @@ import { Container, Grid } from '@substrate/ui-components';
 
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import Tab from 'semantic-ui-react/dist/commonjs/modules/Tab';
+import Dropdown from 'semantic-ui-react/dist/commonjs/modules/Dropdown';
+import Menu from 'semantic-ui-react/dist/commonjs/collections/Menu';
 
 import { ManageAccounts } from './ManageAccounts';
 
 interface Props extends RouteComponentProps {}
 
+const accountManagementOptions = [
+  {
+    key: 'Edit',
+    text: 'Edit',
+    value: 'Edit'
+  },
+  {
+    key: 'Create',
+    text: 'Create',
+    value: 'Create'
+  },
+  {
+    key: 'Restore',
+    text: 'Restore',
+    value: 'Restore'
+  }
+];
+
+const addressManagementOptions = [
+  {
+    key: 'Lookup',
+    text: 'Lookup',
+    value: 'Lookup'
+  },
+  {
+    key: 'Add',
+    text: 'Add',
+    value: 'Add'
+  }
+];
+
 export class Identity extends React.PureComponent<Props> {
   render () {
-    const panes = [
-      { menuItem: 'Manage Accounts', render: () => <ManageAccounts {...this.props} /> }
-    ];
-
     return (
       <Container>
         <Grid>
           <Grid.Row stretched>
-            <Grid.Column width={10}>
-              <Tab panes={panes} menu={{ borderless: true }} />
-            </Grid.Column>
-            <Grid.Column width={6}>
-              nothing else
+            <Grid.Column width={12}>
+              <Menu>
+                <Dropdown
+                    placeholder='Manage Accounts'
+                    fluid
+                    selection
+                    options={accountManagementOptions}
+                  />
+                <Dropdown
+                    placeholder='Manage Addresses'
+                    fluid
+                    selection
+                    options={addressManagementOptions}
+                  />
+              </Menu>
             </Grid.Column>
           </Grid.Row>
         </Grid>
--- packages/identity-app/src/ManageAccounts.tsx
+++ packages/identity-app/src/ManageAccounts.tsx
@@ -3,37 +3,13 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import React from 'react';
-import Dropdown from 'semantic-ui-react/dist/commonjs/modules/Dropdown';
 
 // import { Create } from './Create';
-
-const accountManagementOptions = [
-  {
-    key: 'Edit',
-    text: 'Edit',
-    value: 'Edit'
-  },
-  {
-    key: 'Create',
-    text: 'Create',
-    value: 'Create'
-  },
-  {
-    key: 'Restore',
-    text: 'Restore',
-    value: 'Restore'
-  }
-];
 
 export class ManageAccounts extends React.PureComponent {
   render () {
     return (
-      <Dropdown
-          placeholder='Manage Accounts'
-          fluid
-          selection
-          options={accountManagementOptions}
-        />
+
     );
   }
 }
