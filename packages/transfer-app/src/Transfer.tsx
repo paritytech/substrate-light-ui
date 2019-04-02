5f3c9afaeec2c57d28a5f53f442d7a8fcfed3ba5
Amaury Martiny <amaury.martiny@protonmail.com>
2019-04-02 20:27:36 +0200

    Add InputAddress from apps

--- packages/transfer-app/src/Transfer.tsx
+++ packages/transfer-app/src/Transfer.tsx
@@ -2,15 +2,10 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import addresses$ from '@polkadot/ui-keyring/observable/addresses';
-import accounts$ from '@polkadot/ui-keyring/observable/accounts';
-import { SubjectInfo } from '@polkadot/ui-keyring/observable/types';
-import { Subscribe } from '@substrate/ui-api';
-import { Container, Grid, Header, InputAddress } from '@substrate/ui-components';
+import InputAddress from '@polkadot/ui-app/InputAddress';
+import { Container, Grid, Header } from '@substrate/ui-components';
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import { combineLatest } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 interface MatchParams {
   currentAccount: string;
@@ -22,6 +17,8 @@ interface Props extends RouteComponentProps<MatchParams> {
 
 export class Transfer extends React.PureComponent<Props> {
   render () {
+    const { match: { params: { currentAccount } } } = this.props;
+
     return (
       <Container>
         <Grid>
@@ -30,27 +27,24 @@ export class Transfer extends React.PureComponent<Props> {
           </Grid.Row>
 
           <Grid.Row>
-            <Subscribe>
-              {combineLatest(accounts$.subject, addresses$.subject)
-                .pipe(
-                  map(([accounts, addresses]) => this.renderDropdown(accounts, addresses))
-                )
-              }
-            </Subscribe>
+            <Grid.Column width='5'>
+              <InputAddress
+                onChange={console.log}
+                type='account'
+                value={currentAccount}
+              />
+            </Grid.Column>
+            <Grid.Column width='5'>
+              <InputAddress
+                onChange={console.log}
+                type='account'
+                value={currentAccount}
+              />
+            </Grid.Column>
+
           </Grid.Row>
-        </Grid>
+        </Grid>;
       </Container>
     );
-  }
-
-  renderDropdown = (accounts: SubjectInfo, addresses: SubjectInfo) => {
-    return <InputAddress
-      accounts={accounts}
-      addresses={addresses}
-      defaultValue={this.props.match.params.currentAccount}
-      fluid
-      placeholder='Send from...'
-      selection
-    />;
   }
 }
