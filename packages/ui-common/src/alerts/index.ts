32015e1ef15d4909cc6d2010789ecd5ee062544e
Amaury Martiny <amaury.martiny@protonmail.com>
2019-04-04 14:55:06 +0200

    Remove Alerts

--- packages/ui-common/src/alerts/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2018-2019 @paritytech/substrate-light-ui authors & contributors
-// This software may be modified and distributed under the terms
-// of the Apache-2.0 license. See the LICENSE file for details.
-
-export * from './alerts';
