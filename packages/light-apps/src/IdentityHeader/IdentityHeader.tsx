4968b1e3a8e77a3de4571902e2545f7053f4555f
YJ <yjkimjunior@gmail.com>
2019-04-05 23:24:02 +0800

    feat: include balance subscription

--- packages/light-apps/src/IdentityHeader/IdentityHeader.tsx
+++ packages/light-apps/src/IdentityHeader/IdentityHeader.tsx
@@ -2,14 +2,13 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// import { Balance } from '@polkadot/types';
+import { Balance } from '@polkadot/types';
 import { stringUpperFirst } from '@polkadot/util';
 import { AppContext } from '@substrate/ui-common';
-import { Address, Menu } from '@substrate/ui-components';
-import Dropdown, { DropdownProps } from 'semantic-ui-react/dist/commonjs/modules/Dropdown';
-
+import { BalanceDisplay, Dropdown, DropdownProps, Menu } from '@substrate/ui-components';
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { Observable, Subscription } from 'rxjs';
 
 import { InputAddress } from './IdentityHeader.styles';
 
@@ -29,7 +28,7 @@ const APP_OPTIONS = [
 interface Props extends RouteComponentProps { }
 
 type State = {
-  isReady: boolean
+  balance?: Balance
 };
 
 export class IdentityHeader extends React.PureComponent<Props, State> {
@@ -38,18 +37,32 @@ export class IdentityHeader extends React.PureComponent<Props, State> {
   context!: React.ContextType<typeof AppContext>; // http://bit.ly/typescript-and-react-context
 
   state: State = {
-    isReady: false
+
   };
+
+  subscription?: Subscription;
+
+  componentDidMount () {
+    this.subscribeBalance();
+  }
+
+  componentDidUpdate (prevProps: Props) {
+    if (prevProps.location.pathname.split('/')[2]
+        !== this.props.location.pathname.split('/')[2]) {
+      this.closeSubscription();
+      this.subscribeBalance();
+    }
+  }
+
+  closeSubscription () {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+      this.subscription = undefined;
+    }
+  }
 
   getAddress = () => {
     return this.props.location.pathname.split('/')[2];
-  }
-
-  getButtonText = () => {
-    const currentLocation = this.getCurrentLocation();
-
-    const to = currentLocation === 'identity' ? 'transfer' : 'identity';
-    return stringUpperFirst(to);
   }
 
   getCurrentLocation = () => {
@@ -81,35 +94,49 @@ export class IdentityHeader extends React.PureComponent<Props, State> {
     history.push(`/${value}/${address}`);
   }
 
+  subscribeBalance = () => {
+    const { api } = this.context;
+    const currentAccount = this.getAddress();
+
+    // Subscribe to sender's balance
+    // FIXME using any because freeBalance gives a Codec here, not a Balance
+    // Wait for @polkadot/api to have TS support for all query.*
+    this.subscription = (api.query.balances.freeBalance(currentAccount) as Observable<Balance>)
+      .subscribe((balance) => this.setState({ balance }));
+  }
+
   render () {
+    const { balance } = this.state;
+
     const address = this.getAddress();
     const currentLocation = this.getCurrentLocation();
 
     return (
-      <Menu height='3rem'>
-        <InputAddress
-          label={null}
-          onChange={this.handleChangeCurrentAccount}
-          type='all'
-          value={address}
-          withLabel={false}
-        />
-        <Address address={address} />
+      <Menu>
+        <Menu.Item>
+          <InputAddress
+            label={null}
+            onChange={this.handleChangeCurrentAccount}
+            type='all'
+            value={address}
+            withLabel={false}
+          />
+        </Menu.Item>
+        <Menu.Item>
+          {this.renderBalance(balance)}
+        </Menu.Item>
         <Dropdown
           item
           onChange={this.handleToggleApp}
           options={APP_OPTIONS}
-          text={currentLocation}
-          value={currentLocation}
+          text={stringUpperFirst(currentLocation)}
+          value={stringUpperFirst(currentLocation)}
           />
      </Menu>
     );
   }
 
-  // renderSummary = (balance: Balance) => {
-  //   const address = this.getAddress();
-  //   const name = this.getName();
-  //
-  //   return <AddressSummary address={address} balance={balance} name={name} />;
-  // }
+  renderBalance (balance?: Balance) {
+    return balance && <BalanceDisplay balance={balance} />;
+  }
 }
