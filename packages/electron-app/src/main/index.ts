0dd937abb1b5a9b92edb7266840e180438cc17c5
YJ <yjkimjunior@gmail.com>
2019-04-23 10:20:22 +0200

    set web preferences

--- packages/electron-app/src/main/index.ts
+++ packages/electron-app/src/main/index.ts
@@ -12,11 +12,32 @@ import { staticPath } from './util/staticPath';
 const { app, BrowserWindow } = electron;
 let mainWindow: Electron.BrowserWindow | undefined;
 
+// https://electronjs.org/docs/tutorial/security#electron-security-warnings
+process.env.ELECTRON_ENABLE_SECURITY_WARNINGS = true;
+
 function createWindow () {
   mainWindow = new BrowserWindow({
     height: 1920,
     resizable: true,
-    width: 1440
+    width: 1440,
+    webPreferences: {
+      allowRunningInsecureContent: false,
+      devTools: true,
+      enableBlinkFeatures: '', // https://electronjs.org/docs/tutorial/security#9-do-not-use-enableblinkfeatures
+      enableRemoteModule: false,
+      experimentalFeatures: false, // Do not set to true
+      contextIsolation: true, // prevent context sharing between renderer<->main processes
+      navigateOnDragDrop: false,
+      nativeWindowOpen: true,
+      nodeIntegration: false, // https://electronjs.org/docs/tutorial/security#2-disable-nodejs-integration-for-remote-content
+      nodeIntegrationInWorker: false, // https://electronjs.org/docs/tutorial/security#2-disable-nodejs-integration-for-remote-content
+      plugins: false,
+      sandbox: true, // isolate all BrowserWindow instance environments
+      webSecurity: true, // https://electronjs.org/docs/tutorial/security#5-do-not-disable-websecurity
+      webviewTag: false, // Associated with `will-attach-webview`
+      safeDialogs: true,
+      safeDialogsMessage: 'Electron consecutive dialog protection was triggered'
+    }
   });
 
   mainWindow.loadURL(
@@ -47,6 +68,17 @@ app.on('activate', function () {
   if (mainWindow === undefined) {
     createWindow();
   }
+});
+
+app.on('web-contents-created', (event, contents) => {
+  contents.on('will-attach-webview', (event, webPreferences, params) => {
+    // Strip away preload scripts if unused or verify their location is legitimate
+    delete webPreferences.preload;
+    delete webPreferences.preloadURL;
+
+    // Disable Node.js integration
+    webPreferences.nodeIntegration = false;
+  });
 });
 
 // Quit when all windows are closed.
