62f3a68d57f5e23e751d975dfce3d4ce1ccf7f48
YJ <yjkimjunior@gmail.com>
2019-05-02 14:13:50 +0200

    fix: version requirements

--- scripts/fetchLatestSubstrate.js
+++ scripts/fetchLatestSubstrate.js
@@ -1,22 +1,17 @@
 // curl https://getsubstrate.io -sSf | bash
 
 const { chmod, existsSync, writeFile, writeFileSync } = require('fs');
-const crypto = require('crypto');
-const download = require('download');
 const fetch = require('node-fetch');
 const path = require('path');
 const { promisify } = require('util');
 const semver = require('semver');
 
-// TODO: figure out if there are version requirements???
-// const {
-//     parity: { version: versionRequirement }
-// } = require('../packages/electron-app/package.json');
+const {
+    substrate: { version: versionRequirement }
+} = require('../packages/electron-app/package.json');
 
 const exec = promisify(require('child_process').exec);
 var spawn = require('child_process').spawn;
-const fsChmod = promisify(chmod);
-const fsWriteFile = promisify(writeFile);
 
 function getOs() {
     if (process.argv.includes('--win')) {
@@ -40,9 +35,7 @@ function getOs() {
 }
 
 const userOs = getOs();
-// Need to handle Windows separately
-const ENDPOINT = userOs === 'darwin' || userOs === 'linux' ? 'https://getsubstrate.io' : null;
-const RUST_ENDPOINT = 'https://sh.rustup.rs';
+const ENDPOINT = 'https://getsubstrate.io';
 
 const STATIC_DIRECTORY = path.join(
     '..',
@@ -51,18 +44,49 @@ const STATIC_DIRECTORY = path.join(
     'static'
 );
 
-// FIXME: only relevant for the version requirements check
 const foundPath = [
     path.join(STATIC_DIRECTORY, 'substrate'),
     path.join(STATIC_DIRECTORY, 'substrate.exe')
 ].find(existsSync);
 
-downloadSubstrate();
+if (foundPath) {
+    // Bundled Parity Substrate was found, we check if the version matches the minimum requirements
+    getBinaryVersion(foundPath)
+        .then(version => {
+            if (!version) {
+                console.log("Couldn't get bundled Parity Substrate version.");
+                return downloadSubstrate();
+            }
+
+            // FIXME: get version requirement
+            if (!semver.satisfies(version, versionRequirement)) {
+                console.log(
+                    'Bundled Parity Substrate %s is older than required version %s',
+                    version,
+                    versionRequirement
+                );
+                return downloadSubstrate();
+            } else {
+                console.log(
+                    'Bundled Parity Substrate %s matches required version %s',
+                    version,
+                    versionRequirement
+                );
+            }
+        })
+        .catch(e => {
+            console.error(e);
+            process.exit(1);
+        });
+} else {
+    // Bundled Parity wasn't found, we download the latest version
+    downloadSubstrate();
+}
 
 // Essentially runs this: https://github.com/paritytech/substrate#on-mac-and-ubuntu step by step.
 // Only for OSX and Linux
 function downloadSubstrate() {
-    fetch('https://getsubstrate.io')
+    fetch(ENDPOINT)
         .then(r => r.text())
         .then(script => {
             // write the script to the same directory
@@ -85,47 +109,8 @@ function getBinaryVersion(binaryPath) {
             if (stderr) throw new Error(stderr);
             return stdout.match(/v\d+\.\d+\.\d+/)[0];
         })
-        .catch(error => console.warn(error.message));
+        .catch(error => {
+            console.error(e);
+            process.exit(1);
+        });
 }
-
-
-
-
-
-
-
-// if (foundPath) {
-//     // Bundled Parity Substrate was found, we check if the version matches the minimum requirements
-//     getBinaryVersion(foundPath)
-//         .then(version => {
-//             if (!version) {
-//                 console.log("Couldn't get bundled Parity Substrate version.");
-//                 return downloadSubstrate();
-//             }
-
-//             if (!semver.satisfies(version, versionRequirement)) {
-//                 console.log(
-//                     'Bundled Parity Substrate %s is older than required version %s',
-//                     version,
-//                     versionRequirement
-//                 );
-//                 return downloadSubstrate();
-//             } else {
-//                 console.log(
-//                     'Bundled Parity Substrate %s matches required version %s',
-//                     version,
-//                     versionRequirement
-//                 );
-//             }
-//         })
-//         .catch(e => {
-//             console.error(e);
-//             process.exit(1);
-//         });
-// } else {
-//     // Bundled Parity wasn't found, we download the latest version
-//     downloadSubstrate().catch(e => {
-//         console.error(e);
-//         process.exit(1);
-//     });
-// }
