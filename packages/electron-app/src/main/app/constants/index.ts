19226dfe2a41a960a567f6e4659fc1210d1e9398
YJ <yjkimjunior@gmail.com>
2019-05-07 17:43:56 +0200

    revert here

---
+++ packages/electron-app/src/main/app/constants/index.ts
@@ -0,0 +1,7 @@
+// Copyright 2018-2019 @paritytech/substrate-light-ui authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+export const IS_PROD = process.env.NODE_EV === 'production';
+export const DEFAULT_WS_PORT = '9944';
+export const TRUSTED_LOOPBACK = '127.0.0.1';
