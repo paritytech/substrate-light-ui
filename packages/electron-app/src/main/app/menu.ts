2765d4df576d48381fd7ecc14055f415bbeb8354
Amaury Martiny <amaury.martiny@protonmail.com>
2019-05-10 15:18:11 +0200

    Add menu, use MemoryRouter

---
+++ packages/electron-app/src/main/app/menu.ts
@@ -0,0 +1,101 @@
+import { app, Menu, shell } from 'electron';
+
+const template = [
+  // { role: 'appMenu' }
+  ...(process.platform === 'darwin' ? [{
+    label: app.getName(),
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      process.platform === 'darwin' ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(process.platform === 'darwin' ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startspeaking' },
+            { role: 'stopspeaking' }
+          ]
+        }
+      ] : [
+          { role: 'delete' },
+          { type: 'separator' },
+          { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forcereload' },
+      { role: 'toggledevtools' },
+      { type: 'separator' },
+      { role: 'resetzoom' },
+      { role: 'zoomin' },
+      { role: 'zoomout' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(process.platform === 'darwin' ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+          { role: 'close' }
+      ])
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { shell.openExternal('https://github.com/paritytech/substrate-light-ui'); }
+      }
+    ]
+  }
+];
+
+export function initMenu () {
+  // @ts-ignore FIXME Not sure why TS is complaining about template here...
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
--- packages/light-apps/src/App.tsx
+++ packages/light-apps/src/App.tsx
@@ -3,7 +3,7 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import { AppContext, ContextGate } from '@substrate/ui-common';
 import { Container, GlobalStyle, Loading, substrateLightTheme } from '@substrate/ui-components';
@@ -14,28 +14,35 @@ import { Content } from './Content';
 import { TopBar } from './TopBar';
 import { TxQueueNotifier } from './TxQueueNotifier';
 
+// Use MemoryRouter for production viewing in file:// protocol
+// https://github.com/facebook/create-react-app/issues/3591
+// FIXME Don't use any, but we have the following error:
+// JSX element type 'Router' does not have any construct or call signatures.
+const Router: any =
+  process.env.NODE_ENV === 'production' ? MemoryRouter : BrowserRouter;
+
 export class App extends React.PureComponent {
   render () {
     return (
       <ContextGate>
         <ThemeProvider theme={substrateLightTheme}>
-          <BrowserRouter>
+          <Router>
             <Container>
               <GlobalStyle />
               <AppContext.Consumer>
                 {({ isReady }) => isReady
                   ? <React.Fragment>
-                      <TopBar />
-                      <Content />
-                    </React.Fragment>
+                    <TopBar />
+                    <Content />
+                  </React.Fragment>
                   : <Loading active>
-                      Connecting to the node...
+                    Connecting to the node...
                     </Loading>}
               </AppContext.Consumer>
               <TxQueueNotifier />
               <Alerts />
             </Container>
-          </BrowserRouter >
+          </Router>
         </ThemeProvider>
       </ContextGate>
     );
