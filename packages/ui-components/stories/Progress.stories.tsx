ef5614d0ae4831b430c1c5ddd26472313b4dc18a
YJ <yjkimjunior@gmail.com>
2019-05-13 17:20:09 +0200

    feat: progress bar  + story

---
+++ packages/ui-components/stories/Progress.stories.tsx
@@ -0,0 +1,26 @@
+// Copyright 2018-2019 @paritytech/substrate-light-ui authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { boolean, number, select, text, withKnobs } from '@storybook/addon-knobs';
+
+import { withTheme } from './customDecorators';
+import { Progress } from '../src/Progress';
+import { WrapperDiv } from '../src/Shared.styles';
+
+let colors = ['blue', 'red', 'green', 'orange', 'yellow']; // Fixme: use the gradients
+
+storiesOf('Progress Bar', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withTheme)
+  .add('Primary', () => (
+    <WrapperDiv>
+      <Progress
+        color={select('color', colors)}
+        disabled={boolean('disable', false)}
+        percent={number('percent complete', 50)}
+         />
+    </WrapperDiv>
+  ));
