8bff478af534787c67371a2a06bb46482880c69d
Amaury Martiny <amaury.martiny@protonmail.com>
2019-06-11 14:31:44 +0200

    chore: Update all @polkadot/* packages (#389)
    * Update dev
    
    * Update all packages
    
    * fix: update extendedReferendaInfo type path

--- packages/governance-app/src/Democracy/Referenda.tsx
+++ packages/governance-app/src/Democracy/Referenda.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { ReferendumInfoExtended } from '@polkadot/api-derive/democracy/referendumInfo';
+import { ReferendumInfoExtended } from '@polkadot/api-derive/type';
 import { Option } from '@polkadot/types';
 import { AppContext } from '@substrate/ui-common';
 import { FadedText, Header, Stacked, Table } from '@substrate/ui-components';
