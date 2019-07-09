878e5115dcc1d420aac739a2469ba697f38094e1
YJ <yjkimjunior@gmail.com>
2019-07-09 18:00:45 +0200

    staking area

--- packages/accounts-app/src/Accounts/StakingOptions.tsx
+++ packages/accounts-app/src/Accounts/StakingOptions.tsx
@@ -2,13 +2,59 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React from 'react';
+import { AppContext } from '@substrate/ui-common';
+import { Card, Container, FadedText, Header, Icon, Step, SubHeader, Stacked, StackedHorizontal, FlexItem } from '@substrate/ui-components';
+import React, { useContext, useState } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 
-interface Props {
-}
+interface Props extends RouteComponentProps {}
 
 export function StakingOptions (props: Props) {
+  const { system: { properties: { tokenSymbol }, chain } } = useContext(AppContext);
+  const [activeStep, setActiveStep] = useState('setup');
+
   return (
-    <div> hello </div>
+    <Container>
+        <Header noMargin>Staking Your {tokenSymbol || 'UNIT'}'s on {chain}.</Header>
+        <FadedText>Staking helps secure the network and also gives you a chance at earning a portion of the rewards, if the Validators you nominate successfully author a block.</FadedText>
+        <SubHeader>Be Careful! You can also get slashed if your Validator misbehaves.</SubHeader>
+      <Margin top />
+        <StackedHorizontal alignItems='stretch'>
+          <FlexItem>
+            <Step.Group vertical>
+              <Step active={activeStep === 'setup'}>
+                <Icon name='truck' />
+                <Step.Content>
+                  <Step.Title>Account Setup</Step.Title>
+                  <Step.Description>Set up the required accounts</Step.Description>
+                  <Step.Content>
+                    You will need to set up a stash and controller.
+                  </Step.Content>
+                </Step.Content>
+              </Step>
+
+              <Step active={activeStep === 'bond'}>
+                <Icon name='payment' />
+                <Step.Content>
+                  <Step.Title>Set Bonding Preferences</Step.Title>
+                  <Step.Description>How much do you want to stake? and more.</Step.Description>
+                </Step.Content>
+              </Step>
+
+              <Step active={activeStep === 'nominate'}>
+                <Icon name='info' />
+                <Step.Content>
+                  <Step.Title>Nominate Validator(s)</Step.Title>
+                  <Step.Description>Nominating your funds to a Validator</Step.Description>
+                  <Step.Content> signals that they are trustworthy.</Step.Content>
+                </Step.Content>
+              </Step>
+            </Step.Group>
+          </FlexItem>
+          <FlexItem>
+            action area
+          </FlexItem>
+        </StackedHorizontal>
+    </Container>
   );
 }
--- packages/accounts-app/src/Accounts/index.tsx
+++ packages/accounts-app/src/Accounts/index.tsx
@@ -2,18 +2,59 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React from 'react';
+import { Menu } from '@substrate/ui-components';
+import React, { useState, useEffect } from 'react';
 import { Redirect, Route, RouteComponentProps, Switch } from 'react-router-dom';
 
 import { AccountsOverview } from './AccountsOverview';
+import { StakingOptions } from './StakingOptions';
+import { AccountsOverviewDetailed } from './AccountsOverviewDetailed';
 
-interface Props extends RouteComponentProps {}
+interface MatchParams {
+  currentAccount: string;
+}
+
+interface Props extends RouteComponentProps<MatchParams> {}
+
+const Options = (props: Props) => {
+  const { history, location, match: { params: { currentAccount } } } = props;
+  const [active, setActive] = useState();
+
+  useEffect(() => {
+    setActive(location.pathname.split('/')[3]);
+  });
+
+  const navToOverview = () => {
+    history.push(`/manageAccounts/${currentAccount}/overview`);
+  };
+
+  const navToBalance = () => {
+    history.push(`/manageAccounts/${currentAccount}/balance`);
+  };
+
+  const navToStaking = () => {
+    history.push(`/manageAccounts/${currentAccount}/staking`);
+  };
+
+  return (
+    <Menu>
+      <Menu.Item active={active === 'overview'} onClick={navToOverview}>View All Accounts</Menu.Item>
+      <Menu.Item active={active === 'balances'} onClick={navToBalance}>Detailed Balances</Menu.Item>
+      <Menu.Item active={active === 'staking'} onClick={navToStaking}>Staking Options</Menu.Item>
+    </Menu>
+  );
+};
 
 export function Accounts (props: Props) {
   return (
-    <Switch>
-      <Route path='/manageAccounts/:currentAccount/overview' component={AccountsOverview} />
-      <Redirect from='/manageAccounts/:currentAccount' to='/manageAccounts/:currentAccount/overview' />
-    </Switch>
+    <React.Fragment>
+      <Route component={Options} />
+      <Switch>
+        <Route path='/manageAccounts/:currentAccount/overview' component={AccountsOverview} />
+        <Route path='/manageAccounts/:currentAccount/balances' component={AccountsOverviewDetailed} />
+        <Route path='/manageAccounts/:currentAccount/staking' component={StakingOptions} />
+        <Redirect from='/manageAccounts/:currentAccount' to='/manageAccounts/:currentAccount/staking' />
+      </Switch>
+    </React.Fragment>
   );
 }
