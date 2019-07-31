74b4170c824b90e63f30f14f23a4fccb47c0b151
YJ <yjkimjunior@gmail.com>
2019-08-01 00:18:40 +0200

    fix: do the Validation dance with Bonding

--- packages/accounts-app/src/Staking/Bond.tsx
+++ packages/accounts-app/src/Staking/Bond.tsx
@@ -2,16 +2,22 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { DerivedFees, DerivedBalances } from '@polkadot/api-derive/types';
+import { DerivedBalances } from '@polkadot/api-derive/types';
 import { Nonce } from '@polkadot/types';
 import { isUndefined } from '@polkadot/util';
-import { AppContext, AlertsContext, StakingContext, TxQueueContext, validate } from '@substrate/ui-common';
-import { AddressSummary, Dropdown, DropdownProps, Header, Input, Stacked, StackedHorizontal, StyledNavButton, SubHeader, WithSpace, WithSpaceAround, WrapperDiv, FadedText, FlexItem } from '@substrate/ui-components';
+import { AppContext, AlertsContext, StakingContext, TxQueueContext, validate, AllExtrinsicData } from '@substrate/ui-common';
+import { AddressSummary, Dropdown, DropdownProps, FadedText, Header, Input, Margin, Stacked, StackedHorizontal, StyledNavButton, SubHeader, WithSpace, WithSpaceAround, WrapperDiv } from '@substrate/ui-components';
 import BN from 'bn.js';
+import { Either, left, right } from 'fp-ts/lib/Either';
+import { fromNullable } from 'fp-ts/lib/Option';
 import React, { useContext, useState, useEffect } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { combineLatest, Subscription, Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
+import Loader from 'semantic-ui-react/dist/commonjs/elements/Loader';
+
+import { Errors } from '../types';
+import { Validation } from '../Validation';
 
 interface MatchParams {
   currentAccount?: string;
@@ -37,16 +43,58 @@ export function Bond (props: Props) {
   const { api, keyring } = useContext(AppContext);
   const { enqueue: alert } = useContext(AlertsContext);
   const { derivedBalanceFees } = useContext(StakingContext);
-  const { enqueue, successObservable } = useContext(TxQueueContext);
+  const { enqueue, errorObservable, successObservable } = useContext(TxQueueContext);
   const [bond, setBond] = useState<BN>(new BN(0));
   const [controllerBalance, setControllerBalance] = useState<DerivedBalances>();
   const [destination, setDestination] = useState<RewardDestinationOption>(rewardDestinationOptions[0]);
   const [loading, setLoading] = useState(false);
   const [nonce, setNonce] = useState<Nonce>();
   const [stashBalance, setStashBalance] = useState<DerivedBalances>();
+  const [status, setStatus] = useState<Either<Errors, AllExtrinsicData>>();
 
   const { history } = props;
   const { controller, stash } = history.location.state;
+
+  const _validate = (): Either<Errors, AllExtrinsicData> => {
+    let errors: Errors = [];
+
+    if (bond.lte(new BN(0))) {
+      errors.push('Bond must be greater than zero!');
+    }
+
+    if (isUndefined(derivedBalanceFees)) {
+      errors.push('Calculating fees...please try again in a bit.');
+    }
+
+    if (isUndefined(stash)) {
+      errors.push('Please select a stash account to bond from.');
+    }
+
+    if (isUndefined(destination)) {
+      errors.push('Please select a reward distribution preference.');
+    }
+
+    if (errors.length) { return left(errors); }
+
+    // WARNING: api on this changed v1 => v2 (before you stake with stash, now with controller)
+    const extrinsic = api.tx.staking.bond(controller, bond, destination.value);
+    const values = validate({
+      amountAsString: bond.toString(),
+      accountNonce: nonce,
+      currentBalance: stashBalance,
+      // @ts-ignore the extrinsic works when testing, not sure why tslint is getting the wrong type here
+      extrinsic,
+      fees: derivedBalanceFees,
+      recipientBalance: controllerBalance,
+      currentAccount: stash,
+      recipientAddress: controller
+    }, api);
+
+    return values.fold(
+      (e: any) => left(errors),
+      (allExtrinsicData: any) => right(allExtrinsicData)
+    );
+  }
 
   // use api.consts when it is availabe in @polkadot/api
   useEffect(() => {
@@ -70,51 +118,40 @@ export function Bond (props: Props) {
   }, [stash, controller]);
 
   useEffect(() => {
+    setStatus(_validate());
+  }, [bond, controllerBalance, nonce, stashBalance]);
+
+  useEffect(() => {
     successObservable.subscribe(onSuccess => {
       setLoading(false);
 
       history.push(`/manageAccounts/${controller}/balances`);
     });
+
+    errorObservable.subscribe(onError => {
+      setLoading(false);
+    });
+
+    return () => {
+      errorObservable.unsubscribe();
+      successObservable.unsubscribe();
+    };
   }, []);
 
   const handleConfirmBond = () => {
-    if (isUndefined(derivedBalanceFees)) {
-      alert({ type: 'error', content: 'calculating fees...please try again in a bit.' });
-      return;
-    }
+    if (isUndefined(controller) || isUndefined(stash)) { return; }
 
-    if (isUndefined(stash)) {
-      return;
-    }
-
-    if (isUndefined(destination)) {
-      return;
-    }
-
-    // WARNING: api on this changed v1 => v2 (before you stake with stash, now with controller)
-    const extrinsic = api.tx.staking.bond(controller, bond, destination.value);
-    const values = validate({
-      amountAsString: bond.toString(),
-      accountNonce: nonce,
-      currentBalance: stashBalance,
-      // @ts-ignore the extrinsic works when testing, not sure why tslint is getting the wrong type here
-      extrinsic,
-      fees: derivedBalanceFees,
-      recipientBalance: controllerBalance,
-      currentAccount: stash,
-      recipientAddress: controller
-    }, api);
-
-    values.fold(
-      (errors: any) => alert({ type: 'error', content: Object.values(errors) }),
-      (allExtrinsicData: any) => {
-        const { extrinsic, amount, allFees, allTotal, recipientAddress: controller } = allExtrinsicData;
-
-        const details = { amount, allFees, allTotal, methodCall: extrinsic.meta.name.toString(), senderPair: keyring.getPair(stash), recipientAddress: controller };
-
-        enqueue(extrinsic, details);
-      }
-    );
+    fromNullable(status)
+      .map(_validate)
+      .map(status => status.fold(
+        (errors: any) => alert({ type: 'error', content: Object.values(errors) }),
+        (allExtrinsicData: any) => {
+          setLoading(true);
+          const { extrinsic, amount, allFees, allTotal, recipientAddress: controller } = allExtrinsicData;
+          const details = { amount, allFees, allTotal, methodCall: extrinsic.meta.name.toString(), senderPair: keyring.getPair(stash), recipientAddress: controller };
+          enqueue(extrinsic, details);
+        }
+      ));
   };
 
   const handleSetBond = ({ currentTarget: { value } }: React.SyntheticEvent<HTMLInputElement>) => !isUndefined(value) ? setBond(new BN(value)) : setBond(new BN(0));
@@ -131,47 +168,71 @@ export function Bond (props: Props) {
     setDestination(rewardDestinationOptions[data.value as number]);
   };
 
-  return (
-    <Stacked>
-      <Header>Bonding Preferences </Header>
-      <StackedHorizontal>
-        <WithSpace><FlexItem><FadedText> From: </FadedText><AddressSummary address={stash} noBalance noPlaceholderName size='small' /></FlexItem></WithSpace>
-        <WithSpace><FlexItem><FadedText> To: </FadedText><AddressSummary address={controller} noBalance noPlaceholderName size='small' /></FlexItem></WithSpace>
-      </StackedHorizontal>
-      <WithSpaceAround>
-        <SubHeader>How much do you wish to stake?</SubHeader>
-        {
-          <Stacked>
-            <WrapperDiv>
-              <Input
-                label='Set Amount to Stake.'
-                onChange={handleSetBond}
-                placholder='The total amount of the Stash balance that will be at stake in any forthcoming eras (rewards are distributed in proportion to stake).'
-                value={bond.toString()}
-              />
-            </WrapperDiv>
-            <StackedHorizontal alignItems='stretch'>
-              <WithSpace><button onClick={() => handleSetBondFromPercent(0.25)}>25%</button></WithSpace>
-              <WithSpace><button onClick={() => handleSetBondFromPercent(0.5)}>50%</button></WithSpace>
-              <WithSpace><button onClick={() => handleSetBondFromPercent(0.75)}>75%</button></WithSpace>
-              <WithSpace><button onClick={() => handleSetBondFromPercent(1)}>100%</button></WithSpace>
-            </StackedHorizontal>
-          </Stacked>
-        }
-      </WithSpaceAround>
-      <WithSpaceAround>
-        <SubHeader> How would you like to have your share of the rewards deposited back to you?</SubHeader>
-        <Dropdown
-          fluid
-          onChange={onSelect}
-          options={rewardDestinationOptions}
-          text={destination.text}
-          value={destination.text}
-        />
-      </WithSpaceAround>
-      <WithSpaceAround>
-        <StyledNavButton onClick={handleConfirmBond}>Confirm</StyledNavButton>
-      </WithSpaceAround>
-    </Stacked>
-  );
+  const renderContent = () => {
+    return (
+      <Stacked>
+        <Header>Bonding Preferences </Header>
+        <StackedHorizontal>
+          <FadedText> Stash: </FadedText>
+          <AddressSummary address={stash} noPlaceholderName orientation='horizontal' size='small' />
+          <Margin left='huge' />
+          <FadedText> Controller: </FadedText>
+          <AddressSummary address={controller} noPlaceholderName orientation='horizontal' size='small' />
+        </StackedHorizontal>
+        <WithSpaceAround>
+          <SubHeader>How much do you wish to stake?</SubHeader>
+          {
+            <Stacked>
+              <WrapperDiv>
+                <Input
+                  label='Set Amount to Stake.'
+                  onChange={handleSetBond}
+                  placholder='The total amount of the Stash balance that will be at stake in any forthcoming eras (rewards are distributed in proportion to stake).'
+                  value={bond.toString()}
+                />
+              </WrapperDiv>
+              <StackedHorizontal>
+                <WithSpace><button onClick={() => handleSetBondFromPercent(0.25)}>25%</button></WithSpace>
+                <Margin left />
+                <WithSpace><button onClick={() => handleSetBondFromPercent(0.5)}>50%</button></WithSpace>
+                <Margin left />
+                <WithSpace><button onClick={() => handleSetBondFromPercent(0.75)}>75%</button></WithSpace>
+                <Margin left />
+                <WithSpace><button onClick={() => handleSetBondFromPercent(1)}>100%</button></WithSpace>
+              </StackedHorizontal>
+            </Stacked>
+          }
+        </WithSpaceAround>
+        <WithSpaceAround>
+          <SubHeader> How would you like to have your share of the rewards deposited back to you?</SubHeader>
+          <Dropdown
+            fluid
+            onChange={onSelect}
+            options={rewardDestinationOptions}
+            text={destination.text}
+            value={destination.text}
+          />
+        </WithSpaceAround>
+        <WithSpaceAround>
+          <StyledNavButton onClick={handleConfirmBond}>Confirm</StyledNavButton>
+        </WithSpaceAround>
+        {status && <Validation value={status} />}
+      </Stacked>
+    );
+  };
+
+  const renderLoading = () => {
+    return (
+      <WrapperDiv>
+        <Stacked>
+          <Loader active inline />
+          <SubHeader>Hang Tight! We Are Submitting Your Bond Request...</SubHeader>
+        </Stacked>
+      </WrapperDiv>
+    );
+  };
+
+  return loading
+    ? renderLoading()
+    : renderContent();
 }
--- packages/accounts-app/src/Staking/StakingOptions.tsx
+++ packages/accounts-app/src/Staking/StakingOptions.tsx
@@ -2,9 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { AppContext } from '@substrate/ui-common';
-import { Card, FadedText, Header, Icon, Margin, Stacked, StackedHorizontal, Step, SubHeader, WithSpaceAround } from '@substrate/ui-components';
-import React, { useContext, useState, useEffect } from 'react';
+import { Card } from '@substrate/ui-components';
+import React from 'react';
 import { Redirect, Route, RouteComponentProps, Switch } from 'react-router-dom';
 
 import { Bond } from './Bond';
@@ -17,29 +16,15 @@ interface MatchParams {
 interface Props extends RouteComponentProps<MatchParams> {}
 
 export function StakingOptions (props: Props) {
-  const { system: { properties: { tokenSymbol }, chain } } = useContext(AppContext);
-  const [activeStep, setActiveStep] = useState();
-
-  useEffect(() => {
-    const { location } = props;
-    const step = location.pathname.split('/')[4];
-    setActiveStep(step);
-  }, []);
-
   return (
-      <Card height='100%'>
-        <Card.Header>
-          <Header noMargin>Staking Your {tokenSymbol || 'UNIT'}'s on {chain}.</Header>
-          <FadedText>Staking helps secure the network and also gives you a chance at earning a portion of the rewards, if the Validators you nominate successfully author a block.</FadedText>
-          <SubHeader>Be Careful! You can also get slashed if your Validator misbehaves.</SubHeader>
-        </Card.Header>
-        <Card.Content>
-          <Switch>
-            <Route path='/manageAccounts/:currentAccount/staking/setup' component={Setup} />
-            <Route path='/manageAccounts/:currentAccount/staking/bond' component={Bond} />
-            <Redirect from='/manageAccounts/:currentAccount/staking/' to='/manageAccounts/:currentAccount/staking/setup' />
-          </Switch>
-        </Card.Content>
-      </Card>
+    <Card height='100%'>
+      <Card.Content>
+        <Switch>
+          <Route path='/manageAccounts/:currentAccount/staking/setup' component={Setup} />
+          <Route path='/manageAccounts/:currentAccount/staking/bond' component={Bond} />
+          <Redirect from='/manageAccounts/:currentAccount/staking/' to='/manageAccounts/:currentAccount/staking/setup' />
+        </Switch>
+      </Card.Content>
+    </Card>
   );
 }
