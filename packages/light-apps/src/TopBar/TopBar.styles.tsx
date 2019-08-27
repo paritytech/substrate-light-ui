0087fd3491c0d30fc6beba5f72d56524f8572ad4
YJ <yjkimjunior@gmail.com>
2019-08-27 19:21:00 +0200

    feat: breadcrumbs component (#528)
    * Update dev
    
    * Update all packages
    
    * fix: update extendedReferendaInfo type path
    
    * feat: welcome screen
    
    * feat: each step
    
    * feat: create stash
    
    * create controller and stash together
    
    * fix(refactor) individual components
    
    * fix: style
    
    * fix: use router
    
    * fix: use sr25519 and addFromUri
    
    * feat: setup nominator
    
    * fix: reword, refactor, lint
    
    * feat: breadcrumbs
    
    * Update packages/ui-components/src/Circle.tsx
    
    * fix: grumbles
    
    * fix: tslint
    
    * fix: yarn.lock
    
    * fix: make it lint ffs

--- packages/light-apps/src/TopBar/TopBar.styles.tsx
+++ packages/light-apps/src/TopBar/TopBar.styles.tsx
@@ -2,18 +2,13 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { Margin, StackedHorizontal, SubHeader } from '@substrate/ui-components';
+import { Circle, Margin, StackedHorizontal, SubHeader } from '@substrate/ui-components';
 import React from 'react';
 
 import { BlockCounterProps, NodeStatusProps } from './types';
 
-const Circle = (fill: string) =>
-  <svg height='10' width='10'>
-    <circle cx='5' cy='5' r='5' fill={fill} />
-  </svg>;
-
-const GreenCircle = () => Circle('#79c879');
-const RedCircle = () => Circle('#ff0000');
+const GREEN = '#79c879';
+const RED = '#ff0000';
 
 export const BlockCounter = ({ blockNumber, chainName }: BlockCounterProps) => (
   <React.Fragment>
@@ -24,7 +19,7 @@ export const BlockCounter = ({ blockNumber, chainName }: BlockCounterProps) => (
 
 export const NodeStatus = ({ isSyncing }: NodeStatusProps) => (
   <StackedHorizontal>
-    {isSyncing.eq(true) ? <RedCircle /> : <GreenCircle />}
+    {isSyncing.eq(true) ? <Circle fill={GREEN} /> : <Circle fill={RED} />}
     <Margin left='small' />
     <p> Status: {isSyncing.eq(true) ? 'Syncing' : 'Synced'} </p>
   </StackedHorizontal>
