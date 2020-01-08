f7224a670a0bcf3e6ea7257fe67cd159054fa539
Amaury Martiny <amaury.martiny@protonmail.com>
2020-01-08 16:12:13 +0100

    feat: Add a browser extension (#698)
    * feat: Add a browser extension
    
    * Fix build script in extension
    
    * Yarn install
    
    * Make build an dpackage work
    
    * Fix CI
    
    * lint --fix
    
    * Update height
    
    * Make no accounts work
    
    * Fix identiity header paths
    
    * yarn install
    
    * Remove console.log
    
    * Copy images
    
    * Fix scrollable
    
    * Update images

---
+++ packages/extension-app/README.md
@@ -0,0 +1,3 @@
+# extension-app
+
+Build a browser extension.
--- packages/extension-app/src/background/index.ts
+++ packages/extension-app/src/background/index.ts
@@ -2,7 +2,4 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export * from './Accounts';
-export * from './AddAccount';
-export * from './ManageAddresses';
-export * from './Overview';
+console.log('background.js Code');
