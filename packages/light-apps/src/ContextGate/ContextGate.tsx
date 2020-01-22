023e1f82d4343b7060ba38dfe4f66eeeb3a6bc4e
Amaury Martiny <amaury.martiny@protonmail.com>
2020-01-22 15:51:56 +0100

    feat(light-apps): Choose provider for different env (#716)
    * feat: Choose provider for different env
    
    * Add typings for chrome and browser vars
    
    * Typo!

--- packages/light-apps/src/ContextGate/ContextGate.tsx
+++ packages/light-apps/src/ContextGate/ContextGate.tsx
@@ -3,6 +3,7 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { WsProvider } from '@polkadot/api';
+import { ProviderInterface } from '@polkadot/rpc-provider/types';
 import {
   AlertsContextProvider,
   ApiContext,
@@ -17,9 +18,54 @@ import React from 'react';
 import { HealthContextProvider } from './context/HealthContext';
 import { HealthGate } from './HealthGate';
 
-// FIXME Use PostMessageProvider once we have an extension
-// https://github.com/paritytech/substrate-light-ui/issues/52
-const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io');
+const ELECTRON_ENV = 'ELECTRON_ENV';
+const EXTENSION_ENV = 'EXTENSION_ENV';
+const BROWSER_ENV = 'BROWSER_ENV';
+type Env = typeof ELECTRON_ENV | typeof EXTENSION_ENV | typeof BROWSER_ENV;
+
+/**
+ * Detect whether light-apps is running in Electron, in an Extension popup, or
+ * as a regular browser webpage.
+ */
+function detectEnvironment(): Env {
+  // Detect ELECTRON_ENV
+  // https://github.com/electron/electron/issues/2288#issuecomment-337858978
+  const userAgent = navigator.userAgent.toLowerCase();
+  if (userAgent.includes(' electron/')) {
+    return ELECTRON_ENV;
+  }
+
+  // Detect EXTENSION_ENV
+  // Chrome extensions have the global `chrome` object, Firefox have the
+  // `browser` one (WebExtension one).
+  if (typeof chrome !== 'undefined' || typeof browser !== 'undefined') {
+    return EXTENSION_ENV;
+  }
+
+  return BROWSER_ENV;
+}
+
+/**
+ * Depending on the environment, the provider we use is different.
+ */
+function getProvider(env: Env): ProviderInterface {
+  switch (env) {
+    case ELECTRON_ENV:
+      // We use the local light node embedded in the electron app
+      return new WsProvider('ws://127.0.0.1:9944');
+    case EXTENSION_ENV:
+      // We use a PostMessageProvider to communicate directly with the
+      // background script
+      // FIXME Use PostMessageProvider once we have an extension
+      // https://github.com/paritytech/substrate-light-ui/issues/52
+      return new WsProvider('wss://kusama-rpc.polkadot.io/');
+    default:
+      // We fallback to the remote node provided by W3F
+      return new WsProvider('wss://kusama-rpc.polkadot.io/');
+  }
+}
+
+const wsProvider = getProvider(detectEnvironment());
 
 export function ContextGate(props: { children: React.ReactNode }): React.ReactElement {
   const { children } = props;
