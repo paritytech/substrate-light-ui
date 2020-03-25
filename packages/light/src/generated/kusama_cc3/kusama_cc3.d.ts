37c725473c6f27083f8e515bf9118744f7c58f99
Amaury Martiny <amaury.martiny@protonmail.com>
2020-03-25 16:38:06 +0100

    fix(light): Update to latest wasm blob (#864)

--- packages/light/src/generated/kusama_cc3/kusama_cc3.d.ts
+++ packages/light/src/generated/kusama_cc3/kusama_cc3.d.ts
@@ -2,12 +2,11 @@
 /* eslint-disable */
 /**
 * Starts the client.
-*
-* You must pass a libp2p transport that supports .
-* @param {any} wasm_ext
-* @returns {any}
+* @param {string} chain_spec 
+* @param {string} log_level 
+* @returns {any} 
 */
-export function start_client(chain_spec: string, wasm_ext: any): any;
+export function start_client(chain_spec: string, log_level: string): any;
 /**
 * A running client.
 */
@@ -15,14 +14,14 @@ export class Client {
   free(): void;
   /**
   * Allows starting an RPC request. Returns a `Promise` containing the result of that request.
-  * @param {string} rpc
-  * @returns {any}
+  * @param {string} rpc 
+  * @returns {any} 
   */
   rpcSend(rpc: string): any;
   /**
   * Subscribes to an RPC pubsub endpoint.
-  * @param {string} rpc
-  * @param {any} callback
+  * @param {string} rpc 
+  * @param {any} callback 
   */
   rpcSubscribe(rpc: string, callback: any): void;
 }
