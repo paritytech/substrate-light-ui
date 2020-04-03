819b0eac3277b04af0939aafc42d47776e533f62
Amaury Martiny <amaury.martiny@protonmail.com>
2020-04-03 12:08:44 +0200

    fix(light-apps): Add modal as per Figma (#876)
    * fix(light-apps): Add modal as per Figma
    
    * Fix topbar too
    
    * Alphabetical

--- packages/light-apps/src/Accounts/Accounts.tsx
+++ packages/light-apps/src/Accounts/Accounts.tsx
@@ -2,17 +2,31 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Modal } from '@substrate/ui-components';
+import React, { useCallback } from 'react';
+import { Route, RouteComponentProps } from 'react-router-dom';
 
 import { AddAccount } from './Add';
 import { AccountsOverview } from './Overview';
 
-export function Accounts(): React.ReactElement {
+type Props = RouteComponentProps;
+
+export function Accounts(props: Props): React.ReactElement {
+  const { history } = props;
+
+  const goToAccounts = useCallback((): void => history.push('/'), [history]);
+
   return (
-    <Switch>
-      <Route path='/accounts/add' component={AddAccount} />
+    <>
+      <Route
+        path='/accounts/add'
+        render={(routeProps): React.ReactElement => (
+          <Modal onClose={goToAccounts} open>
+            <AddAccount {...routeProps} />
+          </Modal>
+        )}
+      />
       <Route component={AccountsOverview} />
-    </Switch>
+    </>
   );
 }
