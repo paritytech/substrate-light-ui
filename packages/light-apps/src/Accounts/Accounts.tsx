b26506c9493325643f5d717cdd8ca0a41de261cf
Amaury Martiny <amaury.martiny@protonmail.com>
2020-04-03 10:51:17 +0200

    fix(light-apps): Add modal as per Figma

--- packages/light-apps/src/Accounts/Accounts.tsx
+++ packages/light-apps/src/Accounts/Accounts.tsx
@@ -2,17 +2,29 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { Modal } from '@substrate/ui-components';
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, RouteComponentProps } from 'react-router-dom';
 
 import { AddAccount } from './Add';
 import { AccountsOverview } from './Overview';
 
-export function Accounts(): React.ReactElement {
+type Props = RouteComponentProps;
+
+export function Accounts(props: Props): React.ReactElement {
+  const { history } = props;
+
   return (
-    <Switch>
-      <Route path='/accounts/add' component={AddAccount} />
+    <>
+      <Route
+        path='/accounts/add'
+        render={(routeProps): React.ReactElement => (
+          <Modal open onClose={(): void => history.push('/')}>
+            <AddAccount {...routeProps} />
+          </Modal>
+        )}
+      />
       <Route component={AccountsOverview} />
-    </Switch>
+    </>
   );
 }
