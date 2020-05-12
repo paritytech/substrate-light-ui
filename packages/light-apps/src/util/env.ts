c70b50139608624ab545ea03c4e9800b870d8086
Amaury Martiny <amaury.martiny@protonmail.com>
2020-05-12 11:20:32 +0200

    Fix detectEnvironment

--- packages/light-apps/src/util/env.ts
+++ packages/light-apps/src/util/env.ts
@@ -2,8 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import extension from '@polkadot/extension-inject/chrome';
-
 const ELECTRON_ENV = 'ELECTRON_ENV';
 const POPUP_ENV = 'POPUP_ENV';
 const TAB_ENV = 'TAB_ENV';
@@ -27,9 +25,17 @@ export function detectEnvironment(): Env {
 
   // Detect POPUP_ENV
   // Chrome extensions have the global `chrome` object, Firefox have the
-  // `browser` one (WebExtension). These are all detected in
-  // `@polkadot/extension-inject`.
-  if (extension) {
+  // `browser` one (WebExtension).
+  // See https://stackoverflow.com/questions/29997428/how-can-i-determine-if-a-chrome-extension-is-in-a-popup-from-the-content-script
+  if (
+    (typeof chrome !== 'undefined' &&
+      chrome.extension &&
+      chrome.extension.getBackgroundPage &&
+      chrome.extension.getBackgroundPage() !== window) ||
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore Browser can indeed be not defined
+    typeof browser !== 'undefined'
+  ) {
     return POPUP_ENV;
   }
 
